8b8ceff70c3822735a216e466da3510ad4917059
Tesals <85995048+Akinlua@users.noreply.github.com>
2023-10-02 19:00:54 +0100

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -1,29 +1,17 @@
 const express = require('express')
 const router = express.Router();
 const multer = require('multer');
-const {Upload} = require("../controller/main")
+const {Upload, getUploads, uploadChunk} = require("../controller/main")
 const fs = require("fs")
 
-// Configure multer for file uploads
-const storage = multer.diskStorage({
-    destination: async function (req, file, cb) {   
-      const uploadDirectory = './public/upload/'  
-       if(!fs.existsSync(uploadDirectory)){
-        fs.mkdirSync(uploadDirectory, {recursive: true})
-       }
-      cb(null, uploadDirectory); // Destination folder for uploaded files
-    },
-    filename: async function (req, file, cb) {
-    console.log(file.originalname) 
-      cb(null,  "Video_"+ Date.now()  + '-' + file.originalname); // File naming convention
-    },
-  });
-   
-const upload = multer({ 
-    storage: storage
-});
 
 
-router.post("/upload", upload.single('video'), Upload)
+
+const storage = multer.memoryStorage(); // Store chunks in memory
+const upload = multer({ storage });
+
+
+router.post("/upload", upload.single('chunk'),uploadChunk)
+router.get('/uploads', getUploads)
 
 module.exports = router
